70d681620d6f9243bdf471d88113ae625e7710e7
Joonas Javanainen <joonas.javanainen@gmail.com>
2014-03-07 10:58:06 +0200

    Restore window manager name hack

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 import XMonad.Config.Gnome (gnomeConfig)
 import XMonad.Hooks.EwmhDesktops (fullscreenEventHook)
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName (setWMName)
 import XMonad.Layout.NoBorders (smartBorders)
 
 myManageHook = composeAll
@@ -23,6 +24,8 @@ myHandleEventHook = fullscreenEventHook
 
 myModMask = mod4Mask
 
+myStartupHook = setWMName "LG3D"
+
 main =
   xmonad $ gnomeConfig
       { focusFollowsMouse = True
@@ -30,4 +33,5 @@ main =
       , layoutHook = smartBorders $ layoutHook gnomeConfig
       , manageHook = myManageHook <+> manageHook gnomeConfig
       , modMask = myModMask
+      , startupHook = startupHook gnomeConfig >> myStartupHook
       }
