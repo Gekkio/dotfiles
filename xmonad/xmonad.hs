dd2894d2dc0a5f93abe9341ef589572750f7ef0c
Joonas Javanainen <joonas.javanainen@gmail.com>
2017-07-11 20:25:45 +0300

    Overhaul xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,38 +1,48 @@
-import XMonad
-import XMonad.Config.Gnome (gnomeConfig)
-import XMonad.Hooks.EwmhDesktops (fullscreenEventHook)
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.SetWMName (setWMName)
-import XMonad.Layout.NoBorders (smartBorders)
+{-# LANGUAGE FlexibleContexts #-}
 
-myManageHook = composeAll
-  [ transience'
-  , windowTypes
-  , titles
-  ]
-  where windowTypes = composeOne
-          [ isSplash -?> doIgnore
-          , isFullscreen -?> doFullFloat
-          , isDialog -?> doCenterFloat
+import XMonad
+import XMonad.Config.Desktop (desktopLayoutModifiers)
+import XMonad.Config.Gnome (gnomeConfig)
+import XMonad.Hooks.ManageHelpers (composeOne, doSideFloat, isDialog, isInProperty, transience, (-?>), Side(CE))
+import XMonad.Hooks.SetWMName (setWMName)
+import XMonad.Layout.Fullscreen (fullscreenSupport)
+import XMonad.Layout.Grid (Grid(..))
+import XMonad.Layout.NoBorders (smartBorders)
+import XMonad.Layout.Tabbed (simpleTabbed)
+
+myLayoutHook = tiled ||| Full ||| simpleTabbed ||| Grid
+  where tiled = Tall 1 (3/100) (3/5)
+
+myManageHook = composeAll [ manageWindow, manageOverrides ]
+  where manageWindow = composeOne
+          [ transience
+          -- Dialogs (e.g. Ubuntu logout dialog)
+          , isDialog -?> doFloat
+          -- Splash screens
+          , isSplash -?> doIgnore
+          ]
+        manageOverrides = composeOne
+          [ className =? "Teensy" -?> doSideFloat CE
           ]
         isSplash = isInProperty "_NET_WM_WINDOW_TYPE" "_NET_WM_WINDOW_TYPE_SPLASH"
-        titles = composeOne
-          [ title =? "Eclipse" -?> doFloat
-          , title =? "Monitor" -?> doFloat
-          ]
-
-myHandleEventHook = fullscreenEventHook
 
 myModMask = mod4Mask
 
 myStartupHook = setWMName "LG3D"
 
-main =
-  xmonad $ gnomeConfig
-      { focusFollowsMouse = True
-      , handleEventHook = myHandleEventHook <+> handleEventHook gnomeConfig
-      , layoutHook = smartBorders $ layoutHook gnomeConfig
-      , manageHook = myManageHook <+> manageHook gnomeConfig
-      , modMask = myModMask
-      , startupHook = startupHook gnomeConfig >> myStartupHook
-      }
+myConfig baseConfig = withSmartBorders $ withFullscreen $ withDesktopLayoutModifiers $ baseConfig
+  { focusFollowsMouse = True
+  , layoutHook = myLayoutHook
+  , manageHook = myManageHook <+> manageHook baseConfig
+  , modMask = myModMask
+  , startupHook = startupHook baseConfig >> myStartupHook
+  }
+  where
+    -- Include support for full screen windows
+    withFullscreen = fullscreenSupport
+    -- Include smart borders (= hide when only one window exists)
+    withSmartBorders c = c { layoutHook = smartBorders $ layoutHook c }
+    -- Include sane desktop behaviour (e.g. avoidStruts for docks/panels)
+    withDesktopLayoutModifiers c = c { layoutHook = desktopLayoutModifiers $ layoutHook c }
+
+main = xmonad $ myConfig $ gnomeConfig
